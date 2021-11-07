03afcd9035b0729a5b35685770e1626cbbc7724c
yamasy1549 <sanae@yamasy.info>
2021-11-07 12:54:43 +0900

    feat(create-vivliostyle-theme): build pdf and webpub by default

--- packages/create-vivliostyle-theme/templates/default/vivliostyle.config.js
+++ packages/create-vivliostyle-theme/templates/default/vivliostyle.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   language: 'en',
   theme: 'theme_print.css',
-  entry: [
-    'example/default.md',
+  entry: ['example/default.md'],
+  output: [
+    'book.pdf',
+    {
+      path: './book',
+      format: 'webpub',
+    },
   ],
-}
+};
